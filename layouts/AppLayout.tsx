9f5e3667624289c526da9f303e81bcd537376b65
tarikciplak <yunus_98_dota@hotmail.com>
2023-09-14 00:11:45 +0300

    add client type logo from header

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import { Logo } from "@/components/Icons/Icons"
 import { TowerAnimation } from "@/components/Icons/TowerAnimation"
 import Text from "@/components/Texts/Text"
+import { ClientImagesByName } from "@/constants/walletConstants";
+import { useWallet } from "@/contexts/WalletProvider";
+import { ClientEnum } from "@/types/types";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
+
+    const { clientType } = useWallet();
+
     return (
         <>
             <TowerAnimation />
@@ -14,6 +22,18 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
                     <Text size='3xl'>Aeroscraper</Text>
                     <Logo className='lg:w-[72px] lg:h-[72px] w-[64px] h-[64px]' />
                 </div>
+                {clientType && (
+                    <div className={`flex items-center absolute ${clientType === ClientEnum.ARCHWAY ? "top-20 right-32" : "top-14 right-40"}`}>
+                        <Text size='lg'>on</Text>
+                        {
+                            clientType == ClientEnum.ARCHWAY ?
+                                <img alt={clientType} src={"/images/archway.svg"} className='w-full h-12 -ml-1' />
+                                :
+                                <img alt={clientType} src={"/images/sei-red.svg"} className='w-full h-24 -ml-2' />
+
+                        }
+                    </div>
+                )}
                 <div>
                     <Text size='xs'>Work in Progress</Text>
                 </div>
