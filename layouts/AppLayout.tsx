fdbd29143f34cd99df1c6eda5cf7eebd37bdc0e0
tarikciplak <yunus_98_dota@hotmail.com>
2023-09-17 18:29:59 +0300

    fixed layout

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -17,7 +17,7 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
             <img src='/images/bg-shape-1.svg' className='fixed right-0 top-0 -z-20 select-none pointer-events-none' alt="tower" />
             <img src='/images/bg-shape-2.svg' className='fixed right-4 top-[420px] -z-20 select-none pointer-events-none' alt="tower" />
             <img src='/images/bg-shape-3.svg' className='fixed right-16 bottom-0 -z-20 select-none pointer-events-none' alt="tower" />
-            <header className='w-full flex flex-col items-end gap-10 pt-8 pb-4 px-8'>
+            <header className='w-full flex flex-col items-end gap-10 pt-8 pb-4 px-8 mb-6'>
                 <div className='flex items-center gap-6'>
                     <Text size='3xl'>Aeroscraper</Text>
                     <Logo className='lg:w-[72px] lg:h-[72px] w-[64px] h-[64px]' />
