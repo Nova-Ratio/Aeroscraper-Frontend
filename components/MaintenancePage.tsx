809262b98368d68a22d9fc9207b9884cad13c171
tarikciplak <yunus_98_dota@hotmail.com>
2023-12-17 20:58:14 +0300

    feat: add maint. in footer comp

--- components/MaintenancePage.tsx
+++ components/MaintenancePage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { InjectiveBackgroundWave, LogoSecondary } from './Icons/Icons'
 import Text from "@/components/Texts/Text"
 import InjectiveStatisticSide from '@/app/app/dashboard/_components/Injective/InjectiveStatisticSide'
+import Link from 'next/link'
 
 const MaintenancePage = () => {
   return (
@@ -14,15 +15,71 @@ const MaintenancePage = () => {
       </header>
 
       <div className='flex justify-between'>
-      <InjectiveStatisticSide basePrice={0} disabledStats />
+        <InjectiveStatisticSide basePrice={0} disabledStats />
 
         <div className='w-1/3 flex justify-end flex-col gap-4'>
           <Text size='5xl' textColor='text-[#E4462D]'>Under Maintenance! Your Site is Getting Updated, Will Be Back Shortly.</Text>
           <Text size='lg' >Our site is undergoing maintenance for an important update. We're working to provide you with a better experience. Thank you for your patience and support.</Text>
         </div>
       </div>
-      <InjectiveBackgroundWave animate className="absolute -bottom-60 left-0 rotate-90 -z-10 md:w-[500px] w-[300px]" />
-
+      <InjectiveBackgroundWave animate className="absolute bottom-20 left-0 rotate-90 -z-10 md:w-[500px] w-[300px]" />
+      <footer className='flex flex-col gap-x-48 gap-y-16 items-top flex-wrap px-20 bg-transparent -mx-20 pr-16 mt-60 pb-24 relative'>
+        <div className='flex items-center gap-6 lg:mt-20'>
+          <LogoSecondary />
+          <Text size="2xl" textColor='text-white'>Aeroscraper</Text>
+        </div>
+        <div className='grid grid-cols-3 gap-40'>
+          <div className='flex flex-col content-start justify-start gap-4'>
+            <Text size="sm" textColor='text-white' weight="font-semibold">Product</Text>
+            <Link href={'https://novaratio.gitbook.io/aeroscraper/aeroscraper/whitepaper'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+              <Text size="sm" textColor='text-white' className="cursor-pointer">Whitepaper</Text>
+            </Link>
+            <Link href={'https://aeroscraper.gitbook.io/aeroscraper/brand-identity/brand-kit'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+              <Text size="sm" textColor='text-white'>Brand Identity</Text>
+            </Link>
+            <Link href={'https://testnet.faucet.injective.network/'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+              <Text size="sm" textColor='text-white'>Injective Faucet</Text>
+            </Link>
+          </div>
+          <div className='flex flex-col content-start justify-start gap-6'>
+            <Text size="sm" weight="font-semibold">Deep dive</Text>
+            <div className='flex flex-col content-start gap-3'>
+              <Link href={'https://aeroscraper.gitbook.io/aeroscraper/definitions-of-aeroscraper/definition-of-name'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Definition of name</Text>
+              </Link>
+              <Link href={'https://aeroscraper.gitbook.io/aeroscraper/definitions-of-aeroscraper/definition-of-icon'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Definition of icon</Text>
+              </Link>
+              <Link href={'https://aeroscraper.gitbook.io/aeroscraper/definitions-of-aeroscraper/definition-of-colors'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Definition of colors</Text>
+              </Link>
+              <Link href={'https://aeroscraper.gitbook.io/aeroscraper/definitions-of-aeroscraper/definition-of-typography'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Definition of typography</Text>
+              </Link>
+              <Link href={'https://aeroscraper.gitbook.io/aeroscraper/'} className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Definition of concept</Text>
+              </Link>
+            </div>
+          </div>
+          <div className='flex flex-col content-start justify-start gap-6'>
+            <Text size="sm" weight="font-semibold">Social</Text>
+            <div className='flex flex-col content-start gap-4'>
+              <Link href={'https://twitter.com/aeroscraper'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>X</Text>
+                <img alt='external-link' src='/images/external-link.svg' className='w-4 h-4' />
+              </Link>
+              <Link href={'https://discord.gg/3R6yTqB8hC'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Discord</Text>
+                <img alt='external-link' src='/images/external-link.svg' className='w-4 h-4' />
+              </Link>
+              <Link href={'https://zealy.io/c/aeroscraper/questboard'} target="_blank" rel="noopener noreferrer" className='hover:scale-105 transition-all flex gap-2'>
+                <Text size="sm" textColor='text-white'>Zealy</Text>
+                <img alt='external-link' src='/images/external-link.svg' className='w-4 h-4' />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
