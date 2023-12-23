919aefcf64c286d232ac7f1d6949399813fccb64
tarikciplak <yunus_98_dota@hotmail.com>
2023-12-23 14:33:35 +0300

    feat: add zealy mission service & view

---
+++ app/api/zealy/missions/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+
+  const result = await fetch("https://api.zealy.io/communities/aeroscraper/quests", {
+    headers: {
+      "x-api-key": `${process.env.NEXT_PUBLIC_ZEALY_API_KEY}`
+    },
+    method: req.method,
+    cache: "no-cache"
+  });
+
+  const data = await result.json()
+
+  if (result.status !== 200) {
+    return NextResponse.json({
+      status: result.status,
+      msg: "There was a problem",
+    });
+  } else {
+    return NextResponse.json(data);
+  }
+}
