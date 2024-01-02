ac197e4f9e0b5aca90baf7f72e284161b18db652
tarikciplak <yunus_98_dota@hotmail.com>
2024-01-03 00:59:57 +0300

    fix: build problem

--- app/api/zealy/missions/route.ts
+++ app/api/zealy/missions/route.ts
@@ -1,9 +1,6 @@
-import { ZealyMission } from "@/components/MissionCard";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const zealyId: any = new URL(req.nextUrl)
-
 
   const missionDatas = await fetch("https://api.zealy.io/communities/aeroscraper/quests", {
     headers: {
@@ -13,19 +10,7 @@ export async function GET(req: NextRequest) {
     cache: "no-cache"
   });
 
-  const missionsData: ZealyMission[] = await missionDatas.json();
-
-  const claimedMissionDatas = await fetch("https://api.zealy.io/communities/aeroscraper/quests", {
-    headers: {
-      "x-api-key": `${process.env.NEXT_PUBLIC_ZEALY_API_KEY}`
-    },
-    method: req.method,
-    cache: "no-cache"
-  });
-
-
-
-
+  const missionsData = await missionDatas.json();
 
   if (missionDatas.status !== 200) {
     return NextResponse.json({
